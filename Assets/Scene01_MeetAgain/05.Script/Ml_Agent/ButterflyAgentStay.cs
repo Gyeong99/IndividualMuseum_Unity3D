91c71c7d7b59cbc71892cd850cc5780993ef1dc9
Gyeong99 <m2440179@naver.com>
2023-12-21 22:03:12 +0900

    20231221_Individual_1001Pm
    Make:
    ButterflyChase AI Model
    ButterflyStay AI Model
    2 types of Training Area (Chase , Stay)

--- Assets/Scene01_MeetAgain/05.Script/Ml_Agent/ButterflyAgentStay.cs
+++ Assets/Scene01_MeetAgain/05.Script/Ml_Agent/ButterflyAgentStay.cs
@@ -5,8 +5,9 @@ using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
 
-namespace MeetAgain {
-    public class ButterflyAgent : Agent
+namespace MeetAgain
+{
+    public class ButterflyAgentStay : Agent
     {
 
         // rBody라는 Rigidbody 함수 정의
@@ -25,24 +26,24 @@ namespace MeetAgain {
         public Transform Target;
         public override void OnEpisodeBegin()
         {
-            
+
             //Agent가 너무 멀리 떨어지면 angularVelocity/velocity=0으로, 위치를 초기 좌표로 리셋
             if (isEndByBreak)
             {
                 this.rBody.angularVelocity = Vector3.zero;
                 this.rBody.velocity = Vector3.zero;
-                this.transform.localPosition = new Vector3(Target.localPosition.x + Random.value * 30 - 15 , Random.value * 3 + 0.3f, Target.localPosition.z + Random.value * 30 - 15);   
+                this.transform.localPosition = new Vector3(Target.localPosition.x + Random.value * 10 - 5, Random.value * 1.5f + 2.2f, Target.localPosition.z + Random.value * 10 - 5);
                 isEndByBreak = !isEndByBreak;
             }
             // Target을 Random.value함수를 활용해서 새로운 무작위 위치에 이동
-            Target.localPosition = new Vector3(Random.value * 5, Random.value * 1 + 2.5f, Random.value * 8 - 3);
+            
             this.spawnPoint = transform.position;
         }
 
         public override void CollectObservations(VectorSensor sensor)       // ml 에이전트의 환경 관찰 및 정보를 주는 것 (쉽게 말해 힘 조절을 해주게 하는 함수이다.)
         {
             // Target/Agent의 위치 정보 수집
-            sensor.AddObservation(Target.localPosition);
+            sensor.AddObservation(Vector3.Distance(this.transform.position, Target.position));
             sensor.AddObservation(this.transform.localPosition);
 
             // Agent의 velocity 정보 수집
@@ -52,10 +53,16 @@ namespace MeetAgain {
         }
 
         public float forceMultiplier = 1;
+        /// <summary>
+        /// OnActionReceived는 FixedUpdate만큼 초당 20프레임 정도 호출된다.
+        /// 모델을 등록하지 않은 교육 상태에서도 동일
+        /// 만약 모델을 등록했다면 OnActionRececived는 FixedUpdate처럼 사용이 가능하다
+        /// 모델이 actionBuffer를 주는 것(OnActionReveived를 호출함)으로 추정이 된다. 자세한건 조사해야함
+        ///</summary>
         public override void OnActionReceived(ActionBuffers actionBuffers)
         {
+
             
-            float distanceFromSpawnPoint = Vector3.Distance(spawnPoint, Target.position);
             // Agent가 Target쪽으로 이동하기 위해 X, Z축으로의 Force를 정의
             Vector3 controlSignal = Vector3.zero;
             controlSignal.x = actionBuffers.ContinuousActions[0];
@@ -66,26 +73,26 @@ namespace MeetAgain {
             // Agent와 Target사이의 거리를 측정
             float distanceToTarget = Vector3.Distance(this.transform.position, Target.position);
             
-            // Target에 도달하는 경우 (거리가 1.42보다 작은 경우) Episode 종료
-            if (distanceToTarget <= 1.42)
-            {
-                SetReward(1.0f);                // 이게 완전한 보상인지 , 1회차 보상인지 조사가 필요
-                EndEpisode();
-            }
 
             // 플랫폼 밖으로 나가면 Episode 종료
 
-             if (distanceToTarget >= 50.0f || this.transform.position.y <= - 0.1f || this.transform.position.y >= 15.0f)
+            if (distanceToTarget >= 6.0f || this.transform.position.y <= -0.1f || this.transform.position.y >= 8.0f)
             {
                 isEndByBreak = !isEndByBreak;
-                SetReward(-0.005f);
+                SetReward(-0.05f);
+                EndEpisode();
+            }
+            else if (distanceToTarget >= 1.0f && distanceToTarget < 5.0f)
+            {
+                SetReward(1.0f);
                 EndEpisode();
             }
 
+               
 
 
         }
 
-        
+
     }
 }
